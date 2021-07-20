dfbd656f6de2d58feba622c62b5f6cd5cd47217c
franciel <francielvictor@gmail.com>
2021-07-19 22:57:06 -0300

    arquivos VdeVinganca .html .js, ok!

--- js/VdeVinganca.js
+++ js/VdeVinganca.js
@@ -1,7 +1,11 @@
 window.addEventListener("load", function(){
-    for(linha = 0; linha < 4; linha+=1){
-        for(coluna = 0; coluna < 4; coluna+=1){
-            document.write("_");
+    for(linha = 0; linha < 6; linha+=1){
+        for(coluna = 0; coluna <= 10; coluna+=1){
+           if((coluna == linha)||(coluna == 10-linha)){
+                document.write("*");
+           }else{
+                document.write("_");
+           }
         }
         document.write("<br>");
     }
