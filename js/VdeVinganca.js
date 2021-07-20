d70442c39dff21fb6fe49a0bf0e67cf82804f45c
franciel <francielvictor@gmail.com>
2021-07-19 21:49:26 -0300

    criacao dos arquivos VdeVinganca .html e .js, e utilizando o  addEventListener()

---
+++ js/VdeVinganca.js
@@ -0,0 +1,8 @@
+window.addEventListener("load", function(){
+    for(linha = 0; linha < 4; linha+=1){
+        for(coluna = 0; coluna < 4; coluna+=1){
+            document.write("_");
+        }
+        document.write("<br>");
+    }
+});
