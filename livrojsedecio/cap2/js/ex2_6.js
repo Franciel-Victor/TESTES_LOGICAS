5b126aa1e371f324735c6862ace94efa5bc29cd5
Franciel Victor <francielvictor@gmail.com>
2023-09-06 17:02:44 -0300

    criação dos arquivos ex2_6 .html e .js

---
+++ livrojsedecio/cap2/js/ex2_6.js
@@ -0,0 +1,10 @@
+function mostrarCobranca(){
+    let inValor = document.getElementById("inValor");
+    let inTempoUso = document.getElementById("inTempoUso");
+    let outValor = document.getElementById("outValor");
+
+    let valor = Number(inValor.value);
+    let tempo = Number(inTempoUso.value);
+
+    //let valorPagar = Math.round(tempo/15);
+}
